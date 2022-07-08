65aecab0e85661a8347f88ec792bf1834183fb08
David <104447335+davidnorris78@users.noreply.github.com>
2022-07-08 14:15:44 +0100

    Added support for Beta Channel
    Added support for Beta Channel in Sparkle.

--- Munki Catalog Browser/AppDelegate.swift
+++ Munki Catalog Browser/AppDelegate.swift
@@ -9,10 +9,15 @@ import Cocoa
 import Sparkle
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, SPUStandardUserDriverDelegate, SPUUpdaterDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
+    }
+    
+    func allowedChannels(for updater: SPUUpdater) -> Set<String> {
+        print("beta")
+        return Set(["beta"])
     }
 
     func applicationDidBecomeActive(_ notification: Notification) {
@@ -26,4 +31,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
+    
+    
 }
